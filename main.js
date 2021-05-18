e433495043467d661fe6881fbf053be4b9d3e6a3
SHARMAMANNAT <83449796+SHARMAMANNAT@users.noreply.github.com>
2021-05-18 11:45:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+name_of_the_student_array =[];
+
+function submit()
+{
+    for(var j=1;j<=4;j++){
+        var name_of_the_student=document.getElementById("name_of_the_student"+j).value;
+        console.log(name_of_the_student);
+name_of_the_student_array.push(name_of_the_student);
+    }
+    var display_student_array=[];
+    console.log(name_of_the_student_array); 
+    var length_of_the_name_of_the_student_array =name_of_the_student_array.length;
+    console.log(length_of_the_name_of_the_student_array)
+    for(var k=0;k<=length_of_the_name_of_the_student_array;k++){
+       display_student_array("<h4>NAME-"+name_of_the_student_array[k]+"</h4>");
+        console.log(display_student_array);
+    }
+document.getElementById("display_name_with_commas").innerHTML=display_student_array;
+var remove_commas=display_student_array.join(" ");
+document.getElementById("display_names_without_commas").innerHTML=remove_commas;
+document.getElementById("submit_button").style.display = "none";
+ document.getElementById("sort_button").style.display = "inline-block";
+ }
+  function sorting()  
+{ 
+    name_of_the_student_array.sort();
+     console.log(name_of_the_student_array); 
+     document.getElementById("display_name").innerHTML = name_of_the_student_array; 
+    }
